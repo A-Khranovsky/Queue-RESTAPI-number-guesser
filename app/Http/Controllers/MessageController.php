6f2495efb75975d80f2b95be8b266ac886e076f5
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-04-30 19:16:40 +0300

    Realized running of a supervisor, handling of jobs

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\MessageJob;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
     public function receiveMessage(Request $request)
     {
+        MessageJob::dispatch($request->message);
         return response($request->message ,200);
     }
 }
